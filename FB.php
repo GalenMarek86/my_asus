aee57a8349e004f4787a485fa4c45bc33ab727f0
Denis <denis.gordovskiy@gmail.com>
2017-06-06 21:46:26 +0300

    first

---
+++ FB.php
@@ -0,0 +1,28 @@
+<?php
+$arrFile = file("d:\\phpless\\file.txt");
+$fizz = $arrFile[0];
+$buzz = $arrFile[1];
+$quantity = $arrFile[2];
+
+function bfstar ($fizz, $buzz, $quantity){
+	
+}
+function numTest ($fizz, $buzz, $quantity){
+	if (($fizz > 0) && ($buzz > 0) && ($quantity > 0)){
+		
+		for ($cikl = 1; $cikl <= $quantity; $cikl++){
+			if (($cikl % $fizz == 0) && ($cikl % $buzz == 0)){
+				echo ($cikl == $quantity) ? "FB." : "FB ";
+			}elseif ($cikl % $buzz == 0){
+				echo ($cikl == $quantity) ? "B." : "B ";
+			}elseif ($cikl % $fizz == 0){
+				echo ($cikl == $quantity) ? "F." : "F ";
+			}else{
+				echo ($cikl == $quantity) ? "$cikl." : "$cikl ";
+			}
+		}
+	//
+	}else	echo "error";
+}
+echo numTest($fizz, $buzz, $quantity);
+?>
